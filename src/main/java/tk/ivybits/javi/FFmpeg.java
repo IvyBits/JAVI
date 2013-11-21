151a3a8f46c51e608a251aa414974ebcf052c92d
Tudor <vulcainus@gmail.com>
2013-11-21 13:42:15 -0500

    Wrap log levels

--- src/main/java/tk/ivybits/javi/FFmpeg.java
+++ src/main/java/tk/ivybits/javi/FFmpeg.java
@@ -1,6 +1,7 @@
 package tk.ivybits.javi;
 
 import com.sun.jna.Native;
+import tk.ivybits.javi.api.log.LogLevel;
 import tk.ivybits.javi.ffmpeg.*;
 
 public class FFmpeg {
@@ -9,6 +10,14 @@ public class FFmpeg {
     public static final LibAVUtil avutil = (LibAVUtil) Native.loadLibrary("avutil-52", LibAVUtil.class);
     public static final LibSWScale swscale = (LibSWScale) Native.loadLibrary("swscale-2", LibSWScale.class);
     public static final LibSWResample swresample = (LibSWResample) Native.loadLibrary("swresample-0", LibSWResample.class);
+
+    public static void setLogLevel(LogLevel level) {
+        avutil.av_log_set_level((level.ordinal() * 8 - 8));
+    }
+
+    public static LogLevel getLogLevel() {
+        return LogLevel.values()[avutil.av_log_get_level() / 8 + 1];
+    }
 
     static {
         // Register all codecs.
