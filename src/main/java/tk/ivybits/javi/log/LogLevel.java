cb20ffc26046d55848ee37bb2ae53d71f3147182
Tudor <vulcainus@gmail.com>
2013-11-23 14:36:44 -0500

    Make LogLevel use less magic internally
    Refs #103 @10m

--- src/main/java/tk/ivybits/javi/log/LogLevel.java
+++ src/main/java/tk/ivybits/javi/log/LogLevel.java
@@ -2,20 +2,46 @@ package tk.ivybits.javi.log;
 
 import tk.ivybits.javi.ffmpeg.LibAVUtil;
 
+/**
+ * Logging level control.
+ *
+ * @version 1.0
+ * @since 1.0
+ */
 public enum LogLevel {
-    LOG_QUIET,
-    LOG_PANIC,
-    LOG_FATAL,
-    LOG_ERROR,
-    LOG_WARNING,
-    LOG_INFO,
-    LOG_VERBOSE,
-    LOG_DEBUG;
+    LOG_QUIET(-8),
+    LOG_PANIC(0),
+    LOG_FATAL(8),
+    LOG_ERROR(16),
+    LOG_WARNING(24),
+    LOG_INFO(32),
+    LOG_VERBOSE(40),
+    LOG_DEBUG(48);
 
-    public static void setLogLevel(LogLevel level) {
-        LibAVUtil.av_log_set_level((level.ordinal() * 8 - 8));
+    private int internal;
+
+    LogLevel(int internal) {
+        this.internal = internal;
     }
 
+    /**
+     * Sets the FFmpeg logging level.
+     *
+     * @param level The level to set to.
+     * @version 1.0
+     * @since 1.0
+     */
+    public static void setLogLevel(LogLevel level) {
+        LibAVUtil.av_log_set_level(level.internal);
+    }
+
+    /**
+     * Fetches the current logging level.
+     *
+     * @return The level.
+     * @version 1.0
+     * @since 1.0
+     */
     public static LogLevel getLogLevel() {
         return LogLevel.values()[LibAVUtil.av_log_get_level() / 8 + 1];
     }
