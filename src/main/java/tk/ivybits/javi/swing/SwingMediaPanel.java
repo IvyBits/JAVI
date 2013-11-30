8a532962784fbb65fdd8a4f50016a830bed47f48
Tudor <vulcainus@gmail.com>
2013-11-29 20:32:39 -0500

    Add LGPL header to sources

--- src/main/java/tk/ivybits/javi/swing/SwingMediaPanel.java
+++ src/main/java/tk/ivybits/javi/swing/SwingMediaPanel.java
@@ -1,5 +1,24 @@
+/*
+ * This file is part of JAVI.
+ *
+ * JAVI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * JAVI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JAVI.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package tk.ivybits.javi.swing;
 
+import com.sun.istack.internal.Nullable;
 import tk.ivybits.javi.media.Media;
 import tk.ivybits.javi.media.MediaHandler;
 import tk.ivybits.javi.media.stream.AudioStream;
@@ -195,7 +214,6 @@ public class SwingMediaPanel extends JPanel {
                 Graphics2D g2d = nextFrame.createGraphics();
                 for (Subtitle subtitle : subtitles) {
                     switch (subtitle.type()) {
-
                         case SUBTITLE_BITMAP:
                             int x = ((BitmapSubtitle) subtitle).x;
                             int y = ((BitmapSubtitle) subtitle).y;
