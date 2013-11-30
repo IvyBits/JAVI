25ccb591036940a6f6ebbcf25af27eb05ffa69b3
Tudor <vulcainus@gmail.com>
2013-11-29 22:21:01 -0500

    Improve debugging tools

--- src/main/java/tk/ivybits/javi/debug/JNADebug.java
+++ src/main/java/tk/ivybits/javi/debug/JNADebug.java
@@ -1,7 +1,24 @@
+/*
+ * This file is part of JAVI.
+ *
+ * JAVI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * JAVI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JAVI.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package tk.ivybits.javi.debug;
 
 import com.sun.jna.Structure;
-import tk.ivybits.javi.debug.StructureUtilities;
 
 public class JNADebug {
     public static final String[] classes = {
@@ -22,14 +39,20 @@ public class JNADebug {
             "avutil.AVClass",
             "avutil.AVFrame",
             "avutil.AVRational",
+            "avcodec.AVPicture",
+            "avcodec.AVSubtitle",
+            "avcodec.AVSubtitleRect"
     };
 
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         for (String clazz : classes) {
-            Class<?> struct = Class.forName("tk.ivybits.javi.ffmpeg." + clazz);
-            Structure victim = (Structure) struct.newInstance();
-            StructureUtilities.printFields(victim);
-            System.out.println();
+            try {
+                Class<?> struct = Class.forName("tk.ivybits.javi.ffmpeg." + clazz);
+                Structure victim = (Structure) struct.newInstance();
+                StructureUtilities.printFields(victim);
+                System.out.println();
+            } catch (Exception e) {
+            }
         }
     }
 }
--- src/main/java/tk/ivybits/javi/debug/NativeSizes.java
+++ src/main/java/tk/ivybits/javi/debug/NativeSizes.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of JAVI.
+ *
+ * JAVI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * JAVI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JAVI.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package tk.ivybits.javi.debug;
 
 import com.sun.jna.Structure;
@@ -10,11 +28,14 @@ public class NativeSizes {
                 "#include <avformat/avformat.h>\n" +
                 "int main() {");
         for (String clazz : JNADebug.classes) {
-            Class<?> struct = Class.forName("tk.ivybits.javi.ffmpeg." + clazz);
-            Structure victim = (Structure) struct.newInstance();
-            StructureUtilities.printFieldsC(victim, System.out);
-            System.out.println("puts(\"\");");
-            System.out.println();
+            try {
+                Class<?> struct = Class.forName("tk.ivybits.javi.ffmpeg." + clazz);
+                Structure victim = (Structure) struct.newInstance();
+                StructureUtilities.printFieldsC(victim, System.out);
+                System.out.println("puts(\"\");");
+                System.out.println();
+            } catch (Exception e) {
+            }
         }
         System.out.println("}");
     }
